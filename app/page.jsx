e328d4b229f428e24c2cc4105fdf300e17e498f5
Thur00 <arthursvinhedo@gmail.com>
2023-11-07 00:42:52 -0300

    adição do template do nav de imagens

--- app/page.jsx
+++ app/page.jsx
@@ -3,16 +3,56 @@
 import styles from './page.module.css'
 import Nav from '@/components/Header'
 import Footer from '@/components/Footer'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.capa}>
-      <p>
-        Bem-vindo ao nosso site dedicado à <br />
-        jornada pelos movimentos artísticos <br />
-        que moldaram a história e <br />
-        inspiram gerações.
-      </p>
-    </div>
+    <>
+      <div className={styles.capa}>
+        <p>
+          Bem-vindo ao nosso site dedicado à <br />
+          jornada pelos movimentos artísticos <br />
+          que moldaram a história e <br />
+          inspiram gerações.
+        </p>
+      </div>
+
+
+      <div className={styles.navImg}>
+        <div className={styles.boxGrande}>
+          <Link href="/" className={styles.imgCumprido}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+          <Link href="/" className={styles.imgQuadrado}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+          <Link href="/" className={styles.imgQuadrado}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+        </div>
+
+        <div className={styles.boxGrande}>
+          <Link href="/" className={styles.imgQuadrado}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+          <Link href="/" className={styles.imgQuadrado}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+          <Link href="/" className={styles.imgCumprido}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+        </div>
+
+        <div className={styles.boxPequeno}>
+          <Link href="/" className={styles.imgEspecial}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+          <Link href="/" className={styles.imgEspecial}>
+            <img src="/background.jpg" alt="" />
+          </Link>
+        </div>
+
+      </div>
+    </>
   )
 }
