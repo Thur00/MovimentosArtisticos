2b718f4cc5bfa4bd987fe5bfbd20e1cf1df14839
Thur00 <arthursvinhedo@gmail.com>
2023-11-11 11:23:04 -0300

    adição da primiera parte do nav responsivo

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,6 +16,29 @@ const open = Open_Sans({
     weight: '400'
 })
 
+const menuShow = () => {
+
+    let menuMobile = document.querySelector('.mobile-menu')
+
+    if (menuMobile.classList.contains('open')) {
+
+        menuMobile.classList.remove('open')
+
+        document.querySelector('.icon').src = "/menuIcon.png"
+
+    }
+
+    else {
+
+        menuMobile.classList.add('open')
+
+        document.querySelector('.icon').src = "/menu-icon-close.jpg"
+
+    }
+
+}
+
+
 function Nav(props) {
 
     return (
@@ -32,7 +55,51 @@ function Nav(props) {
                     </div>
                 </div>
                 <nav className="navegacao">
-                    <ul className={open.className}>
+
+                    <div className="nav-header">
+
+                        <ul className={open.className}>
+                            <li>
+                                <Link href="/">Início</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/renascimento">Renascimento</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/barroco">Barroco</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/romantismo">Romantismo</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/neoclassicismo">Neoclassicismo</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/rococo">Rococó</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/realismo">Realismo</Link>
+                            </li >
+                            <li>
+                                <Link href="/paginas/impressionismo">Impressionismo</Link>
+                            </li>
+                            <li>
+                                <Link href="/paginas/abstracionismo">Abstracionismo</Link>
+                            </li>
+                        </ul>
+
+                    </div>
+
+                    <div className="mobile-menu-icon">
+
+                        <button onClick={menuShow} className="btn-icon"><img className="icon" src="/menuIcon.png" alt="Menu Icon" /></button>
+
+                    </div>
+                </nav>
+
+                <div className="mobile-menu">
+
+                    <ul>
                         <li>
                             <Link href="/">Início</Link>
                         </li>
@@ -61,7 +128,9 @@ function Nav(props) {
                             <Link href="/paginas/abstracionismo">Abstracionismo</Link>
                         </li>
                     </ul>
-                </nav>
+
+                </div>
+
             </UserLogadoProvider>
         </>
 
